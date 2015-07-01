1102b652309a24b2bc1489ce80a96c1ff2c87afb
Gerard Morera <grdmorera@gmail.com>
2015-07-01 11:41:03 +0100

    starting heroku config

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source "https://rubygems.org"
 gem 'sinatra'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
+ruby '2.2.1'
 
 group :development, :test do
   gem 'capybara'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,5 @@
 
-ENV['RACK_ENV'] = 'test'
-
 require File.join(File.dirname(__FILE__), '..', 'app/app.rb')
-
 require './app/data_mapper_setup.rb'
 require 'capybara'
 require 'capybara/rspec'
@@ -11,18 +8,17 @@ require 'tilt/erb'
 
 Capybara.app = BookmarkManager
 
+ENV['RACK_ENV'] = 'test'
+
 
 RSpec.configure do |config|
   config.include Capybara::DSL
 
   config.expect_with :rspec do |expectations|
-
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
-
   config.mock_with :rspec do |mocks|
-
     mocks.verify_partial_doubles = true
   end
 
